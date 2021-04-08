e9555c2d4fbbad99452334a3f4fe8d18a79623d5
Wala Zitoun <wala.zitoun@gmail.com>
2021-04-08 13:57:38 +0200

    correction regex

--- Front_end/JS/formulaire.js
+++ Front_end/JS/formulaire.js
@@ -1,5 +1,20 @@
 "use strict";
 
+let nom = document.getElementById("nom");
+let prenom = document.getElementById("prenom");
+let code =  document.getElementById("code");
+let ville = document.getElementById("ville");
+let mail =  document.getElementById("mail");
+let pays = document.getElementById('pays');
+let conditionsCheck = document.getElementById('conditionsCheck');
+
+
+let mailRegEx = new RegExp("[^@]+@[^@]+\.[a-zA-Z]{2,3}");
+let textRegEx = new RegExp("[a-zA-Z]{2,}");
+let numberRegEx = new RegExp("[0-9]{1,}");
+
+
+
 let preOrder = JSON.parse(localStorage.getItem('preOrder'));
 
 // afficher le prix total dans le récapitulatif  
@@ -13,18 +28,15 @@ total.textContent = `Prix total TTC : ${(preOrder.total)} €`;
 //  Envoyer les valeurs du formulaire dans l'api et récuperer l'orderId
 
 let orderUrl = `http://127.0.0.1:3000/api/teddies/order`;
-let nom = document.getElementById("nom");
-let prenom = document.getElementById("prenom");
-
 
 function sendToServer(){
   let data = {
      contact : {
-     firstName:  document.getElementById("nom").value,
-     lastName : document.getElementById("prenom").value,
-     address :  document.getElementById("code").value,
-     city : document.getElementById("ville").value,
-     email :  document.getElementById("mail").value,
+     firstName:  nom.value,
+     lastName : prenom.value,
+     address :  code.value,
+     city :  ville.value,
+     email :  mail.value,
     },
     products : preOrder.id
   };
@@ -64,12 +76,39 @@ function sendToServer(){
 let form = document.getElementById('formulaire');
 let btnSend = document.getElementById('btnSend');
 
-btnSend.addEventListener('click', ()=> {
-  if (form.checkValidity() == true) {
-    sendToServer();
+function checkIsValid(item, regEx) {
+  if (!regEx.test(item.value)) {
+    item.classList.add('is-invalid');
+    return false;
   } else {
-  // dire à bootstrap qu'on a rempli et qu'on souhaite qu'il nous mis à jour le design
-  form.classList.add('was-validated');
+    item.classList.remove('is-invalid');
+    item.classList.add('is-valid');
+    return true;
+  }
+}
+
+function checkBoxIsValid(checkBox) {
+  if (!checkBox.checked ) {
+    checkBox.classList.add('is-invalid');
+    return false;
+  } else {
+    checkBox.classList.remove('is-invalid');
+    checkBox.classList.add('is-valid');
+    return true;
+  }
+}
+
+btnSend.addEventListener('click', ()=> {
+  let mailValid = checkIsValid(mail, mailRegEx);
+  let nomValid = checkIsValid(nom, textRegEx) ;
+  let prenomValid = checkIsValid(prenom, textRegEx);
+  let paysValid = checkIsValid(pays, textRegEx);
+  let villeValid = checkIsValid(ville, textRegEx);
+  let codeValid = checkIsValid(code, numberRegEx);
+  let conditionsCheckValid =  checkBoxIsValid(conditionsCheck);
+
+  if (mailValid && nomValid && prenomValid && paysValid && villeValid && codeValid && conditionsCheckValid) {
+    sendToServer();
   }
 });
 
